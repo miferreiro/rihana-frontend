4438d11c3e841fa07ff27fcbf0da5f8d4beb9363
miferreiro <miguel.ferreiro.diaz@gmail.com>
2021-08-25 13:30:25 +0200

    Adds the service to get the information of an user
    Creates the service to manage users and defines the method to get the
    information of a specific user. In addition, the structures, Users and
    UserInfo, are created to handle the data obtained in the application.

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,51 @@
+/*
+ * RIHANA Frontend
+ *
+ * Copyright (C) 2021 David A. Ruano Ordás, José Ramón Méndez Reboredo,
+ * Miguel Ferreiro Díaz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ */
+
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {environment} from "../../environments/environment";
+import {Users} from "../models/Users";
+import {UserInfo} from "./entities/UserInfo";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsersService {
+
+	constructor(private http: HttpClient) {
+	}
+
+	getUser(login: string): Observable<Users> {
+		return this.http.get<UserInfo[]>(`${environment.restApi}/user/${login}`).pipe(
+			map(this.mapUserInfo.bind(this))
+		);
+	}
+
+	private mapUserInfo(userInfo: UserInfo): Users {
+		return {
+			login: userInfo.login,
+			password: userInfo.password,
+			role: userInfo.role
+		};
+	}
+}
